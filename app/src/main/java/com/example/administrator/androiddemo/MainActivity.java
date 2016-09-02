ff45922e12c24f37511bc01ce2884979f969dfdd
githubLins <1203624432@qq.com>
2016-09-02 18:28:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/androiddemo/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.administrator.androiddemo;
+
+import android.content.SharedPreferences;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+    public static String MyFileName="myfile";
+
+    private final static String SharedPreferencesFileName = "config";
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        preferences = getSharedPreferences(SharedPreferencesFileName,
+                MODE_PRIVATE);
+        editor = preferences.edit();
+        Button btnWrite = (Button) findViewById(R.id);
+        Button btnRead = (Button) findViewById(R.id.ButtonRead);
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OutputStream out=null;
+                try {
+                    if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+                        File file=Environment.getExternalStorageDirectory();
+                        File myfile=new File(file.getCanonicalPath()+"/"+MyFileName);
+                        FileOutputStream fileOutputStream=new FileOutputStream(myfile);
+                        out=new BufferedOutputStream(fileOutputStream);
+                        String content="2014011339 lifalin";
+                        try {
+                            out.write(content.getBytes(StandardCharsets.UTF_8));
+                        }finally {
+                            if (out!=null)
+                                out.close();
+                        }
+                    }
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputStream in=null;
+                try{
+                    if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+                        File file=Environment.getExternalStorageDirectory();
+                        File myfile=new File(file.getCanonicalPath()+"/"+MyFileName);
+                        FileInputStream fileInputStream=new FileInputStream(myfile);
+                        int c;
+                        StringBuilder stringBuilder=new StringBuilder("");
+                        try{
+                            while ((c=in.read())!=-1){
+                                stringBuilder.append((char)c);
+                            }
+                            Toast.makeText(MainActivity.this,stringBuilder.toString(),Toast.LENGTH_LONG).show();
+                        }finally {
+                            if (in!=null)
+                                in.close();
+                        }
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+}
